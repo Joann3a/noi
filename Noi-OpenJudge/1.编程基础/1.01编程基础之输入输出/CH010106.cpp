3568e0b71474a3ee76760bd1e7ebfa5215287b70
joann3a <2796581773@qq.com>
2017-07-23 08:21:32 +0800

    06:空格分隔输出

---
+++ Noi-OpenJudge/1.编程基础/1.01编程基础之输入输出/CH010106.cpp
@@ -0,0 +1,17 @@
+//Author: Calvin Wang
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	char a;
+	int b;
+	float c;
+	double d;
+	 
+	cin >> a >> b >> c >> d;
+	
+	printf("%c %d %.6f %.6f",a,b,c,d);
+	return 0; 
+} 
