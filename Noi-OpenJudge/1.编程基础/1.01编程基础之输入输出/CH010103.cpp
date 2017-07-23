2c57da1c9ebb07117d1e9db1ac7a88b650fb7201
joann3a <2796581773@qq.com>
2017-07-23 08:16:54 +0800

    03:对齐输出

---
+++ Noi-OpenJudge/1.编程基础/1.01编程基础之输入输出/CH010103.cpp
@@ -0,0 +1,14 @@
+//Author: Calvin Wang
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	int a,b,c; 
+	cin >> a >> b >> c;
+	
+	printf("%8d %8d %8d",a,b,c);
+	return 0; 
+} 
