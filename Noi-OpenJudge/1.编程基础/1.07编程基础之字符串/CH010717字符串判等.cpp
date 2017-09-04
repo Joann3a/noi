7c0dda105b77880082e8b04b9b56b69b02ff2e79
joann3a <2796581773@qq.com>
2017-09-04 14:42:33 +0800

    CH010717字符串判等(updateed)

--- Noi-OpenJudge/1.编程基础/1.07编程基础之字符串/CH010717字符串判等.cpp
+++ Noi-OpenJudge/1.编程基础/1.07编程基础之字符串/CH010717字符串判等.cpp
@@ -1,62 +1,37 @@
-#include <iostream>
-#include <cstring>
-#include <cctype>
-#include <algorithm>
-
+#include<iostream>
+#include<algorithm>
+#include<cctype>
 using namespace std;
-
 int main()
 {
-	string s1,s2,s11,s22;
-	int n,j;
-	getline(cin,s1);
-	getline(cin,s2);
-	n=s1.size();
+	string an="",bn="",ao,bo;
+	getline(cin,ao);
+	getline(cin,bo);
+	int i;
 	
-	j=0;
-	for(int i=0;i<n;i++)
+	for(i=0;i<ao.size();i++)
 	{
-		if(s1[i]!=' ') 
+		if(isalpha(ao[i]))
 		{
-			if(s1[i]>='A' && s1[i]<='Z') 
-			s1[i]=s1[i]+'a'-'A'; 
-			s11[i]=s1[i];
-			j++;
-		}
-	}
-
-		
-	j=0;
-	n=s2.size();
-	for(int i=0;i<n;i++)
-	{
-		if(s2[i]!=' ') 
-		{
-			if(s2[i]>='A' && s2[i]<='Z') 
-			s2[i]=s2[i]+'a'-'A'; 
-			s22[i]=s2[i];
-			j++;
+			if(ao[i]>='A'&&ao[i]<='Z')
+				ao[i]+='a'-'A';
+			an+=ao[i];
 		}
 	}	
-
 	
-	int n1,n2;
-	n1=s11.size();
-	n2=s22.size();
-	if(n1!=n2) {
-		cout <<"NO";
-		return 0;
-	} 
-	for(int i=0;i<n1;i++)
+	for(i=0;i<bo.size();i++)
 	{
-		if(s11[i]!=s22[i])
+		if(isalpha(bo[i]))
 		{
-		cout <<"NO";
-		return 0;
+			if(bo[i]>='A'&&bo[i]<='Z')
+				bo[i]+='a'-'A';
+			bn+=bo[i];
 		}
 	}
+	if(an==bn)
+		cout<<"YES";
+	else
+		cout<<"NO";
 	
-	cout <<"YES";
-	return 0; 
-	
+	return 0;
 }
